65a98e5d30bd98547bbc9a8437de5072d1310cee
ColdWindScholar <3590361911@qq.com>
2026-07-17 18:08:40 +0800

    hwc: set default redroid_fps to 30

--- hwcomposer/hwcomposer.cpp
+++ hwcomposer/hwcomposer.cpp
@@ -111,7 +111,7 @@ static int hwc_device_open(const struct hw_module_t* module, const char* name,
         struct hw_device_t** device)
 {
     bool stream_open = property_get_bool("ro.boot.use_redroid_stream",0);
-    int32_t redroid_fps = property_get_int32("ro.boot.redroid_fps",15);
+    int32_t redroid_fps = property_get_int32("ro.boot.redroid_fps",30);
     if (redroid_fps < 1){ redroid_fps = 15; }
     if (stream_open != nullptr){
     ALOGI("hwc_open, streaming enabled");
