90c2765ebd9103a0b9dca93405a07d990749b6a2
ColdWindScholar <3590361911@qq.com>
2026-07-17 22:17:59 +0800

    hwc: set default redroid_fps to 30

--- hwcomposer/hwcomposer.cpp
+++ hwcomposer/hwcomposer.cpp
@@ -257,7 +257,7 @@ static int hwc_event_control(struct hwc_composer_device_1* dev, int disp,
                              int event, int enabled) {
     redroid_hwc_device_t* hwc_dev = static_cast<redroid_hwc_device_t *>(dev);
    std::unique_lock<std::mutex> lock(hwc_dev->hwc_mutex);
-    int err = -EINVAL
+    int err = -EINVAL;
     if (event == 0){
     hwc_dev->vsync_enabled = enabled != 0;
      err = 0;
@@ -307,4 +307,4 @@ static int redroid_vsync_thread_loop(redroid_hwc_device* dev){
 
   ALOGI("vsync thread exiting");
   return 0;
-}
+}}
