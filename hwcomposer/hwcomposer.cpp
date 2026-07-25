d43b3c94688bb76d15b6cab6781c3a2a225460e6
ColdWindScholar <3590361911@qq.com>
2026-07-25 23:15:36 +0800

    update version to 1.2

--- hwcomposer/hwcomposer.cpp
+++ hwcomposer/hwcomposer.cpp
@@ -45,7 +45,7 @@ hwc_module_t HAL_MODULE_INFO_SYM = {
     .common = {
         .tag = HARDWARE_MODULE_TAG,
         .version_major = 1,
-        .version_minor = 1,
+        .version_minor = 2,
         .id = HWC_HARDWARE_MODULE_ID,
         .name = "hwcomposer module for Nredroid",
         .author = "ColdWindScholar",
@@ -282,7 +282,7 @@ static int hwc_device_open(const struct hw_module_t* module, const char* name,
         /* pdev == dev-> device*/
         /* initialize the procs */
         dev->device.common.tag = HARDWARE_DEVICE_TAG;
-        dev->device.common.version = HWC_DEVICE_API_VERSION_1_0;
+        dev->device.common.version = HWC_DEVICE_API_VERSION_1_2;
         dev->device.common.module = const_cast<hw_module_t*>(module);
         dev->device.common.close = hwc_device_close;
         dev->device.prepare = hwc_prepare;
